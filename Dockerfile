e101891305436d9bb6df09371af28775b22cff36
Unknwon <u@gogs.io>
2016-03-26 16:24:38 -0400

    Docker for peach

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM alpine:3.3
+MAINTAINER u@gogs.io
+
+# Install system utils & runtime dependencies
+ADD https://github.com/tianon/gosu/releases/download/1.7/gosu-amd64 /usr/sbin/gosu
+RUN chmod +x /usr/sbin/gosu \
+ && apk --no-cache --no-progress add ca-certificates bash git s6 curl socat
+
+COPY . /app/peach/
+WORKDIR /app/peach/
+RUN ./docker/build.sh
+
+# Configure LibC Name Service
+COPY docker/nsswitch.conf /etc/nsswitch.conf
+
+# Configure Docker Container
+VOLUME ["/data/peach"]
+EXPOSE 5555
+ENTRYPOINT ["docker/start.sh"]
+CMD ["/bin/s6-svscan", "/app/peach/docker/s6/"]
---
+++ docker/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -x
+set -e
+
+# Set temp environment vars
+export GOPATH=/tmp/go
+export PATH=${PATH}:${GOPATH}/bin
+
+# Install build deps
+apk --no-cache --no-progress add --virtual build-deps go gcc musl-dev
+
+# Init go environment to build
+mkdir -p ${GOPATH}/src/github.com/peachdocs/
+ln -s /app/peach/ ${GOPATH}/src/github.com/peachdocs/peach
+cd ${GOPATH}/src/github.com/peachdocs/peach
+go get -v 
+mv ${GOPATH}/bin/peach .
+
+# Cleanup GOPATH
+rm -r $GOPATH
+
+# Remove build deps
+apk --no-progress del build-deps
+
+# Create user
+adduser -H -D -g 'Peach Docs' peach -h /data/peach -s /bin/bash && passwd -u peach
